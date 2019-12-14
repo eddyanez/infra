d533466f0586d043e815252f1b8624aacc98ee34
edd <edd@MacBook-Pro-de-edd.local>
2019-12-14 10:55:01 -0600

    Cuarto comentari22o

--- bash/build.sh
+++ bash/build.sh
@@ -1,4 +1,4 @@
-wget -o packer.zip https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip
+wget -O packer.zip https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip
 unzip packer.zip
 ./packer validate deployments/template.json
 ./packer build deployments/template.json
