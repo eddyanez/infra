71cbf409c44b0ba318ca9a950b5f8ed66748697d
edd <edd@MacBook-Pro-de-edd.local>
2019-12-14 13:46:49 -0600

    Con el Terraformx1000

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -1,4 +1,11 @@
 export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-snapp-$ARTIFACT_ID\") | .id")
+export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-demop-$ARTIFACT_ID\") | .id")
+echo $DIGITALOCEAN_TOKEN
+echo $TF_VAR_image_id
+cd terraform
+terraform init
+terraform apply -input=false  -auto-approve
+
 echo $TF_VAR_image_id
 cd terraform
 terraform init
