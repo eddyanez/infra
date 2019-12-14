f966dde849344bacb5a1fa00cd972c63a3f42958
edd <edd@MacBook-Pro-de-edd.local>
2019-12-14 13:57:27 -0600

    Con Ayuda de Norman

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -1,12 +1,8 @@
-export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-snapp-$ARTIFACT_ID\") | .id")
-export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-demop-$ARTIFACT_ID\") | .id")
 echo $DIGITALOCEAN_TOKEN
+echo $ARTIFACT_ID
+echo 'curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-snapp-$ARTIFACT_ID\") | .id"'
+export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name==\"tec-snapp-$ARTIFACT_ID\") | .id")
 echo $TF_VAR_image_id
 cd terraform
 terraform init
 terraform apply -input=false  -auto-approve
-
-echo $TF_VAR_image_id
-cd terraform
-terraform init
-terraform apply -input=false -auto-approve
